d52f7896c1aa205df4311658e9ab4ac3f3b4cb6d
VictorMA18 <vmamanian@unsa.edu.pe>
2024-07-04 22:39:23 -0500

    Actualizando el main para la implementacion de la insercion , busqueda , reemplazar la palabra de un texto , Las palabras dependiendo de un tipo de prefijo en este caso wa y tambien las primeras 2 palabras que tienen la mayor frecuencia

--- Trie/Main.java
+++ Trie/Main.java
@@ -1,4 +1,6 @@
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
   public static void main(String[] args) {
@@ -17,9 +19,27 @@ public class Main {
     trie.insert("wish");
     trie.insert("wit");
     trie.insert("yo");
-    List<String> topKWords = trie.findTopKFrequentWords(2);
-    System.out.println("Top 2 " + "words are: " + topKWords);
-    System.out.println(trie.searchByPrefix("wa"));
+    
+    System.out.println("Representacion del trie: \n");
+
     trie.draw();
+
+    System.out.println("\nSe encuentra la palabra win en el trie : " + trie.search("win"));
+    System.out.println("Se encuentra la palabra lose en el trie : " + trie.search("lose"));
+
+    // Reemplazar palabras en un texto
+    String text = "I wish to walk and win, but war and want make it hard.";
+    Map<String, String> replacements = new HashMap<>();
+    replacements.put("walk", "stroll");
+    replacements.put("war", "conflict");
+    replacements.put("wish", "desire");
+
+    String replacedText = trie.replaceWordsInText(text, replacements);
+    System.out.println("Texto Original: " + text);
+    System.out.println("Texto Reemplazado: " + replacedText);
+
+    List<String> topKWords = trie.findTopKFrequentWords(2);
+    System.out.println("Las primeras 2 palabras con la mayor frecuencia son: " + topKWords );
+    System.out.println("Las palabras con el prefijo wa son : " + trie.searchByPrefix("wa") + "\n");
   }
 }
